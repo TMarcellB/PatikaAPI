a6add9dd9e8dc753a01f8ba94730ecd4ad51f8df
Marcell Tulák <tulakm@kkszki.hu>
2024-12-06 11:06:30 +0100

    JsonIgnore

--- PatikaAPI/Controllers/KezelController.cs
+++ PatikaAPI/Controllers/KezelController.cs
@@ -1,12 +1,32 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PatikaAPI.Models;
+using System.Text.Json.Serialization;
 
 namespace PatikaAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class KezelController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            using(var context = new PatikaContext())
+            {
+                try
+                {
+                    List<Kezel> kezels = context.Kezels.ToList();
+                    return Ok(kezels);
 
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+
+        }
+        
     }
 }
